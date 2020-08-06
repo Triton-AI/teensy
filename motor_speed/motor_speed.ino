5613e355cbbd1e5945ad691c492de6de6b554d43
Ian D <ianjdelaney@gmail.com>
2020-08-05 20:45:32 -0700

    fixed control loop period

--- motor_speed/motor_speed.ino
+++ motor_speed/motor_speed.ino
@@ -12,14 +12,17 @@ boolean motor_start = false;
 const byte pin_a = 2;   //for encoder pulse A
 const byte pin_b = 3;   //for encoder pulse B
 const byte pin_c = 4;   //for encoder pulse B
+const int interruptPeriod = 0.01;  // Period of interrupt service routine, unit is in seconds
 
-int encoder = 0;
+int encoderA = 0;
+int encoderB = 0;
+int encoderC = 0;
 int m_direction = 0;
 int sv_speed = 100;     //this value is 0~255
-double pv_speed = 0;
+double avg_speed = 0;
 double set_speed = 0;
 double set_steer = 0; 
-double e_speed = 0; //error of speed = set_speed - pv_speed
+double e_speed = 0; //error of speed = set_speed - avg_speed
 double e_speed_pre = 0;  //last error of speed
 double e_speed_sum = 0;  //sum error of speed
 double pwm_pulse = 0;     //this value is 0~255
@@ -38,14 +41,28 @@ void setup() {
 //  pinMode(pin_bwd,OUTPUT);
 //  pinMode(pin_pwm,OUTPUT);
   attachInterrupt(digitalPinToInterrupt(pin_a), detect_a, RISING);
+  attachInterrupt(digitalPinToInterrupt(pin_b), detect_b, RISING);
+  attachInterrupt(digitalPinToInterrupt(pin_c), detect_c, RISING);
   // start serial port at 9600 bps:
   Serial.begin(9600);
   //--------------------------timer setup
   noInterrupts();           // disable all interrupts
   TCCR1A = 0;
   TCCR1B = 0;
-  timer1_counter = 59286;   // preload timer 65536-16MHz/256/2Hz (34286 for 0.5sec) (59286 for 0.1sec)
-
+ 
+  // preload timer 65536-16MHz/256/2Hz (34286 for 0.5sec) (59286 for 0.1sec)
+  switch (interruptPeriod) {
+    case 0.01:
+      timer1_counter = 65911;  // 66536 - (65911) = 625*.0625*256 = 0.01s 
+    case 0.1:
+      timer1_counter = 60286; // 66536 - (60286) = 6250*.0625*256 = 0.1s
+      break;
+    case 0.5:
+      timer1_counter = 34286; // 65536 - (34286) = 31250 *.0625*256 = 0.5s
+      break;
+    default:
+      timer1_counter = 59286; // 0.1s
+  }
   
   TCNT1 = timer1_counter;   // preload timer
   TCCR1B |= (1 << CS12);    // 256 prescaler 
@@ -81,7 +98,7 @@ void loop() {
     motor_start = false;
   }
   if (inputSt.substring(0,10) == "poll_speed"){
-    Serial.println(pv_speed);
+    Serial.println(avg_speed);
   }
   if (inputSt.substring(0,13) == "poll_throttle"){
     Serial.println(throttlePWM);
@@ -112,24 +129,41 @@ void loop() {
 }
     
 void detect_a() {
-  encoder+=1; //increasing encoder at new pulse
+  encoderA+=1; //increasing encoder at new pulse
   m_direction = digitalRead(pin_b); //read direction of motor
+}
+
+void detect_b() {
+  encoderB+=1; //increasing encoder at new pulse
+  m_direction = digitalRead(pin_c); //read direction of motor
+}
+
+void detect_c() {
+  encoderC+=1; //increasing encoder at new pulse
+  m_direction = digitalRead(pin_a); //read direction of motor
 }
 ISR(TIMER1_OVF_vect)        // interrupt service routine - tick every 0.1sec
 {
   TCNT1 = timer1_counter;   // set timer
-  pv_speed = 60.0*(encoder)/0.1;  //calculate motor speed, unit is ticks/s
-  encoder=0;
+  a_speed = 60.0*(encoderA)/interruptPeriod;  //calculate motor speed, unit is ticks/s
+  b_speed = 60.0*(encoderA)/interruptPeriod;  //calculate motor speed, unit is ticks/s
+  c_speed = 60.0*(encoderA)/interruptPeriod;  //calculate motor speed, unit is ticks/s
+  
+  avg_speed = (a_speed + b_speed + c_speed)/3;  // Average of 3 encoder phase speeds
+  
+  encoderA=0;
+  encoderB=0;
+  encoderC=0;
   //print out speed
 //  if (Serial.available() <= 0) {
 //    Serial.print("speed");
-//    Serial.println(pv_speed);         //Print speed (ticks/s) value
+//    Serial.println(avg_speed);         //Print speed (ticks/s) value
 //    }
 
 
   //PID program
   if (motor_start){
-    e_speed = set_speed - pv_speed;
+    e_speed = set_speed - avg_speed;
     pwm_pulse = e_speed*kp + e_speed_sum*ki + (e_speed - e_speed_pre)*kd;
     e_speed_pre = e_speed;  //save last (previous) error
     e_speed_sum += e_speed; //sum of error
@@ -143,7 +177,6 @@ ISR(TIMER1_OVF_vect)        // interrupt service routine - tick every 0.1sec
     pwm_pulse = 0;
   }
   
-
   //update new speed
   if (pwm_pulse <255 & pwm_pulse >0){
     analogWrite(pin_pwm,pwm_pulse);  //set motor speed  
